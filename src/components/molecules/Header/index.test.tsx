5506676d0c9f642736d94f305fec7434a2e98799
Hepsko <Adrian778@op.pl>
2022-09-20 13:12:04 +0200

    add tests  Link, Header

--- src/components/molecules/Header/index.test.tsx
+++ src/components/molecules/Header/index.test.tsx
@@ -1,9 +1,10 @@
 import "@testing-library/jest-dom";
 import { Header } from "./";
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 
-describe("Calculator", () => {
-  it("renders a calculator", () => {
-    render(<Header showSidebar={() => {}} />);
+describe("Header molecule component test", () => {
+  const showSidebar = jest.fn();
+  it("renders a Header", () => {
+    render(<Header showSidebar={showSidebar} />);
   });
 });
