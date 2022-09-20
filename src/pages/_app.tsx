add77ed61c2fc675682bbf775ebd109fb46868ad
Hepsko <Adrian778@op.pl>
2022-09-20 12:06:58 +0200

    add envs policy

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,15 @@ import { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "react-query";
 
 const App = ({ Component, pageProps }: AppProps) => {
-  const queryClient = new QueryClient();
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+        retry: false,
+        staleTime: 5 * 60 * 1000,
+      },
+    },
+  });
 
   return (
     <QueryClientProvider client={queryClient}>
